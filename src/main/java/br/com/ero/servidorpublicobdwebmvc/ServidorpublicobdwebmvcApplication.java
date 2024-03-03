f428e9901ec0b6f4cc83f7cdb323d3b2298c131f
Lucas Lima <lucaserolima@gmail.com>
2024-03-02 22:23:00 -0400

    First Commit

---
+++ src/main/java/br/com/ero/servidorpublicobdwebmvc/ServidorpublicobdwebmvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.servidorpublicobdwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorpublicobdwebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorpublicobdwebmvcApplication.class, args);
+	}
+
+}
